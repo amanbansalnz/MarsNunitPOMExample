fcce392bc381768d80fb855b4af08081c53380f9
Aman Bansal <aman.bansal@gamesys.co.uk>
2023-08-13 17:09:17 +0100

    first commit

---
+++ MarNunitPOMExmaple/Tests/EducationTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.AspNetCore.Routing;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using test.Pages;
+using test.Pages.components;
+using test.Steps;
+using test.Utils;
+
+namespace test
+{
+    public class EducationTest : GobalHelper
+    {
+
+        LoginSteps loginSteps;
+        HomePageSteps homePageSteps;
+        EducationSteps educationSteps;
+
+        public EducationTest()
+        {
+            loginSteps = new LoginSteps();
+            homePageSteps = new HomePageSteps();
+            educationSteps = new EducationSteps();
+
+        }
+
+
+
+        [Test]
+        public void givenLoggedInAndAddingNewEductaion_whenAddEducation_thenEducationIsAdded()
+        {
+            loginSteps.doLogin();
+
+            homePageSteps.validateIsLoggedIn();
+            Thread.Sleep(2000);
+            homePageSteps.clickOnEducationTab();
+
+            educationSteps.addEducation();
+        }
+
+
+    }
+
+}
+
---
+++ MarNunitPOMExmaple/Utils/GlobalHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace test.Utils
+{
+	public class GobalHelper
+	{
+        public static IWebDriver driver;
+
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost:5000");
+            //Thread.Sleep(1000);
+        }
+
+
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
+
